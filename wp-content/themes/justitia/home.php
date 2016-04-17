f04fa2d8dd14f059f5eecebc8379c9755569d95c
Udit Desai <desaiuditd@gmail.com>
2016-04-16 21:40:26 -0700

    configures bbpress forum search

--- wp-content/themes/justitia/home.php
+++ wp-content/themes/justitia/home.php
@@ -8,18 +8,21 @@
  */
 get_header();
 
-$rtp_content_grid_class = apply_filters( 'rtp_set_full_width_grid_class', 'large-12 columns rtp-full-width-grid' );
+$rtp_content_grid_class = apply_filters( 'rtp_set_full_width_grid_class', 'large-8 columns rtp-full-width-grid' );
 $rtp_content_class = ' rtp-content-section ' . $rtp_content_grid_class . ' rtp-blog-post ';
 ?>
 
 	<section id="content" role="main" class="<?php echo esc_attr( $rtp_content_class ); ?>">
 		<?php rtp_hook_begin_content(); ?>
 
-		<input type="text" name="search" placeholder="Search" />
+		<article <?php post_class( 'clearfix rtp-post-box' ); ?>>
 
-		<?php if (is_user_logged_in()) { ?>
-			<button href="#">
-		<?php } ?>
+			<?php echo do_shortcode('[bbp-search-form]'); ?>
+
+			<?php if (is_user_logged_in()) { ?>
+
+			<?php } ?>
+		</article>
 
 		<?php rtp_hook_end_content(); ?>
 	</section><!-- #content -->
